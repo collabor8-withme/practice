84746be8fc84196feead3b9097f672ced1e62581
ciocoin <132557114+liwanruuu@users.noreply.github.com>
2023-08-19 18:50:27 +0800

    Add files via upload

---
+++ liwanruu/commonjs/.eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  //   "off" or 0 - 关闭规则
+  //   "warn" or 1 - 将规则视为一个警告（不会影响退出码）,只警告，不会退出程序
+  //   "error" or 2 - 将规则视为一个错误 (退出码为1)，报错并退出程序
+ rules: {
+    //"no-unused-vars": 'error',
+    "no-multiple-empty-lines": 'error' ,// 不允许有连续多行空行(关闭规则)
+    "semi":'error', 
+    // override configuration set by extending "eslint:recommended"
+    "no-empty": 'warn',
+    "no-cond-assign": 'error', 
+    // 禁用基础配置汇总的规则
+    "for-direction": "off",
+ }
+}
